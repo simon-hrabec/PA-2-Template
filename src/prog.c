6f335325ffc7611a6d33538702dda663d60675a1
Šimon Hrabec <simon.hrabec@gmail.com>
2017-10-13 11:40:34 +0200

    Load 4 numbers and print whether they form square

--- src/prog.c
+++ src/prog.c
@@ -12,7 +12,24 @@ bool isSquare(Point p1, Point p2, Point p3, Point p4){
 
 int main(int argc, char const *argv[])
 {
-	Point p1;
-	printf("%d\n", isSquare(p1,p1,p1,p1));
+	const int pointCount = 4;
+	Point pArr[pointCount];
+
+	// Load 4 points
+	for(int i = 0; i < pointCount; i++){
+		if (scanf("%d", &pArr[i].x) != 1 ||
+			scanf("%d",&pArr[i].y) != 1) {
+			printf("Error loading numbers");
+			return 0;
+		}
+	}
+
+	// Check whether they form a square
+	if (isSquare(pArr[0], pArr[1], pArr[2], pArr[3])){
+		printf("Square\n");
+	} else {
+		printf("Not a square\n");
+	}
+
 	return 0;
 }
