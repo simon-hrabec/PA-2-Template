1a9c30a46e40c655491a3501549d6b46b8572160
Šimon Hrabec <simon.hrabec@gmail.com>
2017-10-13 11:33:54 +0200

    Create basic square function

--- src/prog.c
+++ src/prog.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+typedef struct Point{
+	int x,y;
+} Point;
+
+bool isSquare(Point p1, Point p2, Point p3, Point p4){
+	return true;
+}
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	Point p1;
+	printf("%d\n", isSquare(p1,p1,p1,p1));
 	return 0;
 }
